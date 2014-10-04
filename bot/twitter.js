5e79ccc7c0129b8f03acfd4367c027bdfc8c9673
Sugendran Ganess <sugendran@sugendran.net>
2014-10-04 09:58:38 +0100

    try catch all the things

--- bot/twitter.js
+++ bot/twitter.js
@@ -1,8 +1,8 @@
 var Task = require('../models/task');
 var taskUtils = require('./task-utils');
-var tu;
+var tu = null;
 var stream = null;
-var terms = ['inktober'];
+var terms = ['inktober', '#inktober'];
 var disallowed = ["\uD83D\uDC49","\uD83D\uDC48","\uD83D\uDC46","\uD83D\uDC46"];
 // jake parker = 13205002
 var canRetweetAlways = ['13205002'];
@@ -18,12 +18,18 @@ setInterval(function () {
   lastRetweets = lastRetweets.filter(function (val) { return val >= now; });
 }, 1000);
 
-module.exports.init = function (plugin) {
-  tu = require('tuiter')(plugin.app.config.twitter);
+function init(plugin) {
+  if (tu == null) {
+    tu = require('tuiter')(plugin.app.config.twitter);
+  }
 
   if (stream !== null) {
     stream.emit('end');
     stream = null;
+    setTimeout(function () {
+      init(plugin);
+    }, 1000);
+    return;
   }
 
   function onReTweet(err) {
@@ -34,55 +40,61 @@ module.exports.init = function (plugin) {
   }
 
   function onTweet(tweet) {
-    // if it's flagged as a retweet or has RT
-    // in there then we probably don't want
-    // to retweet it again.
-    if (tweet.retweeted) {
-        return;
-    }
-    if (tweet.text.indexOf("RT") !== -1) {
-        return;
-    }
-    var txt = tweet.text.toLowerCase();
-    if (terms.indexOf(txt) !== -1) {
-      return;
-    }
-
-    for (var i=0, ii=disallowed.length; i<ii; i++) {
-      if (txt.indexOf(disallowed[i]) !== -1) {
+    try {
+      // if it's flagged as a retweet or has RT
+      // in there then we probably don't want
+      // to retweet it again.
+      if (tweet.retweeted) {
+          return;
+      }
+      if (tweet.text.indexOf("RT") !== -1) {
+          return;
+      }
+      var txt = tweet.text.toLowerCase();
+      if (terms.indexOf(txt) !== -1) {
         return;
       }
+
+      for (var i=0, ii=disallowed.length; i<ii; i++) {
+        if (txt.indexOf(disallowed[i]) !== -1) {
+          return;
+        }
+      }
+
+
+      var date = null;
+      try {
+        date = Date.parse(tweet.created_at);
+      } catch (e) {
+        date = Date.now();
+      }
+      var hasEntities = tweet.entities.urls.length > 0;
+      if ( hasEntities &&
+           (
+            (canRetweetAlways.indexOf(tweet.id_str) !== -1) ||
+            (date && date <= endDate && date >= startDate)
+           )
+         ) {
+        lastRetweets.push(Date.now());
+        // console.log("Retweeting: " + tweet.text);
+        // note we're using the id_str property since
+        // javascript is not accurate for 64bit ints
+        tu.retweet({
+            id: tweet.id_str
+        }, onReTweet);
+      }
+
+      var task = new Task({
+        source: "twitter",
+        sourceId: tweet.id_str,
+        payload: tweet
+      });
+      taskUtils.saveTask('http://127.0.0.1:' + plugin.app.config.ports.api, task);
     }
-
-
-    var date = null;
-    try {
-      date = Date.parse(tweet.created_at);
-    } catch (e) {
-
+    catch (e) {
+      console.log("failed to do things with the tweet");
+      console.log(e.message);
     }
-    var hasEntities = tweet.entities.urls.length > 0;
-    if ( hasEntities &&
-         (
-          (canRetweetAlways.indexOf(tweet.id_str) !== -1) ||
-          (date && date <= endDate && date >= startDate)
-         )
-       ) {
-      lastRetweets.push(Date.now());
-      // console.log("Retweeting: " + tweet.text);
-      // note we're using the id_str property since
-      // javascript is not accurate for 64bit ints
-      tu.retweet({
-          id: tweet.id_str
-      }, onReTweet);
-    }
-
-    var task = new Task({
-      source: "twitter",
-      sourceId: tweet.id_str,
-      payload: tweet
-    });
-    taskUtils.saveTask('http://127.0.0.1:' + plugin.app.config.ports.api, task);
   }
 
 
@@ -94,6 +106,8 @@ module.exports.init = function (plugin) {
     stream.on('tweet', onTweet);
   });
 };
+
+module.exports.init = init;
 
 module.exports.health = function () {
   return lastRetweets.length + ' retweets per hour';
